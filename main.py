5bd2b70e6df2bb287809654f89ff293772a7cc24
LucasT <tressoldi.lucas@gmail.com>
2024-02-06 15:40:38 -0300

    create the main function logic to calc the average grades

--- main.py
+++ main.py
@@ -6,3 +6,17 @@ sa = gspread.service_account(filename="credentials.json")
 sh = sa.open("Engenharia de Software - Desafio LucasTressoldi")
 
 worksheet = sh.worksheet("engenharia_de_software")
+
+total_classes = int(worksheet.acell("A2").value[-2:]) * 0.25
+sheet_size = len(worksheet.col_values(1)) + 1
+default_naf = 0
+
+for row in range(4, sheet_size):
+  print(f"Updating row: {row}")
+  missed_classes = int(worksheet.acell(f"C{row}").value)
+
+  if not total_classes >= missed_classes:
+    worksheet.update(range_name=f"G{row}:H{row}", values=[["Reprovado por Falta", f"{default_naf}"]])
+    continue
+
+  situation_calc(worksheet, row, average_grade_calc(worksheet, row), default_naf)
